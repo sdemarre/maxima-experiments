fdaf3575d2aeb032acfb742954038eb6919e854e
serge de marre <sdemarre@gmail.com>
2016-09-18 22:29:17 +0200

    better handling of unsupported platforms

--- share/quicklisp/quicklisp-setup.lisp
+++ share/quicklisp/quicklisp-setup.lisp
@@ -10,30 +10,38 @@
       t)))  ;; maxima wants t or nil in "if" expressions
 
 (defun $load_installed_quicklisp ()
-  #-(or gcl quicklisp)
-  (with-output-to-string (s)
-    (let ((*standard-output* s))
-      (when ($quicklisp_installed_p)
-	(let ((quicklisp-setup-file (quicklisp-setup-file)))
-	  (when quicklisp-setup-file
-	    (load quicklisp-setup-file)))))))
+  (if ($platform_supports_quicklisp)
+      (when  (not ($quicklisp_already_loaded))
+	(with-output-to-string (s)
+	  (let ((*standard-output* s))
+	    (when ($quicklisp_installed_p)
+	      (let ((quicklisp-setup-file (quicklisp-setup-file)))
+		(when quicklisp-setup-file
+		  (load quicklisp-setup-file)))))))
+      (merror (intl:gettext "platform doesn't support quicklisp"))))
 
 (defun $load_quicklisp_init ()
-  ;;#-(or gcl quicklisp)
-  ;; quicklisp is not installed yet, so install it
-  (with-output-to-string (s)
-    (let ((*standard-output* s))
-     (let ((maxima-sharedir (probe-file *maxima-sharedir*)))
-       (if maxima-sharedir
-	   (let ((quicklisp-file (merge-pathnames "quicklisp/quicklisp-init.lisp" maxima-sharedir)))
-	     (if (probe-file quicklisp-file)
-		 (load quicklisp-file)
-		 (merror (intl:gettext "Couldn't initialize quicklisp"))))
-	   (merror (intl:gettext "Couldn't initialize quicklisp"))))))
-  #+gcl(merror (intl:gettext "gcl not yet supported")))
+  (if ($platform_supports_quicklisp)
+      ;; quicklisp is not installed yet, so install it
+      (with-output-to-string (s)
+	(let ((*standard-output* s))
+	  (let ((maxima-sharedir (probe-file *maxima-sharedir*)))
+	    (if maxima-sharedir
+		(let ((quicklisp-file (merge-pathnames "quicklisp/quicklisp-init.lisp" maxima-sharedir)))
+		  (if (probe-file quicklisp-file)
+		      (load quicklisp-file)
+		      (merror (intl:gettext "Couldn't initialize quicklisp"))))
+		(merror (intl:gettext "Couldn't initialize quicklisp"))))))
+      (merror (intl:gettext "platform doesn't support quicklisp")))
 
 (defun $install_quicklisp ()
   (funcall (find-symbol "INSTALL" (find-package :quicklisp-quickstart)) :path (merge-pathnames *maxima-quicklisp-path* (user-homedir-pathname))))
+
+(defun $platform_supports_quicklisp ()
+  (not (find :gcl *features*)))
+
+(defun $quicklisp_already_loaded ()
+  (find :quicklisp *features*))
 
 (defun $quickload (package-name)
   (progn
